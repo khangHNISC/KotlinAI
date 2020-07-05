1f595e32aac9190711c4ea6f5f7a6286b23c7760
khangle1996 <leduckhang1996@gmail.com>
2020-07-05 10:57:49 +0700

    reImplement Structure of graph, Problem

--- src/main/java/playing/Structure.kt
+++ src/main/java/playing/Structure.kt
@@ -1,8 +1,20 @@
 package playing
 
-class State
+class State(val stateName: String) {
+    override fun equals(other: Any?): Boolean {
+        return stateName == (other as State).stateName
+    }
 
-class Action(val DestState: State)
+    override fun hashCode(): Int {
+        return stateName.hashCode()
+    }
+
+    override fun toString(): String {
+        return "State(stateName='$stateName')"
+    }
+}
+
+class Action(val destState: State, val cost: Int)
 
 abstract class Problem(private val initial: State, private val goal: List<State>) {
 
@@ -18,23 +30,61 @@ abstract class Problem(private val initial: State, private val goal: List<State>
     open fun value(state: State) {}
 }
 
+open class Graph(
+        private val graphDict: Map<State, Map<State, Int>>,
+        private val directed: Boolean = true
+) {
+    private val graphDictConstructed: MutableMap<State, MutableMap<State, Int>> = mutableMapOf()
+
+    init {
+        if (!directed) makeUndirected()
+    }
+
+    private fun makeUndirected() {
+        for (a in graphDict.keys) {
+            for ((b, dist) in graphDict[a]?.entries!!) {
+                connect1(a, b, dist)
+                connect1(b, a, dist)
+            }
+        }
+    }
+
+    private fun connect1(fromState: State, toState: State, distance: Int) {
+        val map = graphDictConstructed.getOrDefault(fromState, mutableMapOf())
+        map[toState] = distance
+        graphDictConstructed[fromState] = map
+    }
+
+    fun get(fromState: State) : MutableMap<State, Int>{
+        return graphDictConstructed.getOrDefault(fromState, mutableMapOf())
+    }
+
+    fun getCost(fromState: State, toState: State) : Int {
+        return graphDictConstructed[fromState]?.get(toState) ?: Int.MAX_VALUE
+    }
+}
+
+class UndirectedGraph(
+        private val graphDict: Map<State, Map<State, Int>>
+) : Graph(graphDict, false)
+
+
 class GraphProblem(
         private val initial: State,
         private val goal: List<State>,
-        private val graph: Map<State, Map<State, Int>>
+        private val graph: Graph
 ) : Problem(initial, goal) {
 
     override fun actions(state: State): Set<State> {
-        return graph[state]?.keys ?: emptySet()
+        return graph.get(state).keys
     }
 
     override fun result(state: State, action: Action): State {
-        return action.DestState
+        return action.destState
     }
 
     override fun pathCost(costSoFar: Int, state1: State, action: Action, state2: State): Int {
-        return costSoFar + (graph[state1]?.get(state2) ?: Int.MAX_VALUE)
-
+        return costSoFar + graph.getCost(state1, state2)
     }
 
 }
---
+++ src/main/java/playing/problem/Romania.kt
@@ -0,0 +1,56 @@
+package playing.problem
+
+import playing.Action
+import playing.GraphProblem
+import playing.State
+import playing.UndirectedGraph
+
+fun main() {
+    val romaniaMap = UndirectedGraph(mutableMapOf(
+            State("Arad") to mutableMapOf(
+                    State("Zerind") to 75,
+                    State("Sibiu") to 140,
+                    State("Timisoara") to 118
+            ),
+            State("Bucharest") to mutableMapOf(
+                    State("Urziceni") to 85,
+                    State("Pitesti") to 101,
+                    State("Giurgiu") to 90,
+                    State("Fagaras") to 211
+            ),
+            State("Craiova") to mutableMapOf(
+                    State("Drobeta") to 120,
+                    State("Rimnicu") to 146,
+                    State("Pitesti") to 138
+            ),
+            State("Drobeta") to mutableMapOf(
+                    State("Mehadia") to 75
+            ),
+            State("Eforie") to mutableMapOf(
+                    State("Hirsova") to 86
+            ),
+            State("Fagaras") to mutableMapOf(
+                    State("Sibiu") to 99
+            ),
+            State("Hirsova") to mutableMapOf(
+                    State("Urziceni") to 98
+            ),
+            State("Iasi") to mutableMapOf(
+                    State("Vaslui") to 92,
+                    State("Neamt") to 87
+            ),
+            State("Lugoj") to mutableMapOf(
+                    State("Timisoara") to 111,
+                    State("Mehadia") to 70
+            ),
+            State("Oradea") to mutableMapOf(
+                    State("Zerind") to 71,
+                    State("Sibiu") to 151
+            ),
+            State("Pitesti") to mutableMapOf(State("Rimnicu") to 97),
+            State("Rimnicu") to mutableMapOf(State("Sibiu") to 80),
+            State("Urziceni") to mutableMapOf(State("Vaslui") to 142))
+    )
+
+    val problem = GraphProblem(State("Arad"), listOf(State("Bucharest")), romaniaMap)
+}
